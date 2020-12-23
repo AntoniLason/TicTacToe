6e4cfe5a591b314d49fc452c9e30f547f891354c
Antoni Lasoń <72211818+AntoniLason@users.noreply.github.com>
2020-12-23 23:04:40 +0100

    NWM

--- TicTacToe.c
+++ TicTacToe.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
 
+
 int main()
+{
+    char tab[3][3];
+
+}
+
+
+//***********************
+
+void ruchgracza()
 {
 
 }
+
+int analizazwycienstwa()
+{
+
+}
+void ruchkomputera()
+{
+
+}
+
+//*******************************
+///NOTATKI
+/*
+CO POTRZEBNE
+
+inicjalizacja planszy
+ruch gracza
+tóch komputera
+analiza zwyciêstwa
+generowanie ruchy komputera
+wyœwietlanie
+
+*/
